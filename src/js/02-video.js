eaada5ab260e282cb089f8a0adaec2f57a0e267b
Svetlana-Melenchenko <129314558+Svetlana-Melenchenko@users.noreply.github.com>
2023-08-11 19:22:55 +0300

    second task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.getElementById('vimeo-player');
+const player = new Player(iframe);
+
+function saveCurrentTime() {
+  player.getCurrentTime().then(currentTime => {
+    localStorage.setItem('videoplayer-current-time', currentTime);
+  });
+}
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+if (savedTime) {
+  player.setCurrentTime(savedTime);
+}
+
+const saveCurrentTimeThrottled = throttle(saveCurrentTime, 1000);
+
+player.on('timeupdate', saveCurrentTimeThrottled, { passive: true });
+
+// маю у другому завданні повідомлення в консолі, але це не помилка наче і все працює.
